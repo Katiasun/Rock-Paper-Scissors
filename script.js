48f0e347350e5e563d49bf1eb67153409c81fe67
Katiasun <katjatolok3@gmail.com>
2024-02-09 10:25:31 +0100

    add variables in file js

---
+++ script.js
@@ -0,0 +1,5 @@
+const computerChoice = document.getElementById("computer-choice");
+const userChoice = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
